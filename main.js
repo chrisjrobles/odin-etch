bfaefb259c0a9bf010f1af4b2381efabf47c03ae
Christopher Robles <crobles@nerdwallet.com>
2022-10-27 20:16:28 -0700

    feat: color hovered cells

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const dimensions = 16;
-const squareSizePx = 50;
-const gapPx = 10;
+const drawGrid = ({gridDimensions = 960, cellCount = 16}) => {
+  const cellSize = Math.floor(gridDimensions / cellCount);
 
-const container = document.getElementById('container');
-
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement('div');
-  row.setAttribute('class', 'row');
-
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement('div');
-    cell.setAttribute('class', 'cell');
-    row.appendChild(cell);
+  const container = document.getElementById('container');
+  for (let i = 0; i < cellCount; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+  
+    for (let j = 0; j < cellCount; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.style.width = `${cellSize}px`;
+      cell.style.height = `${cellSize}px`;
+      cell.addEventListener('mouseover', () => {
+        cell.classList.add('hoveredCell');
+      });
+      row.appendChild(cell);
+    }
+  
+    container.appendChild(row);
   }
-
-  container.appendChild(row);
 }
+
+drawGrid({cellCount: 80});
